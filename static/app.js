c2cf126982c94402129c1015986bc684860294f7
tvdaredevil <34181607+tvdaredevil@users.noreply.github.com>
2018-09-15 18:37:21 -0400

    AP is just amazing! All hail our leader, our dearest. How would I do anything without his grace watching over me, every second. #AP_IS_LOVE #AP_IS_LIFE <3

--- static/app.js
+++ static/app.js
@@ -1,4 +1,4 @@
-function initMap() {
+window.initMap = _ => {
     // The location of Uluru
     var uluru = {lat: 33.4, lng: -75.5};
     // The map, centered at Uluru
@@ -6,6 +6,4 @@ function initMap() {
         document.getElementById('map'), {zoom: 4, center: uluru});
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: uluru, map: map});
-
-
 }
